e4cce22650218a9c39de7bff6f29e3c9e5255fad
NORMANDIA CAKRA WIJAYA <54388146+cakra1@users.noreply.github.com>
2026-01-13 18:39:09 +0700

    db.py

--- db.py
+++ db.py
@@ -1,10 +1,14 @@
 import pymysql
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_db():
     return pymysql.connect(
-        host="localhost",
-        user="root",
-        password="animeotaku",
-        database="web_py",
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
         cursorclass=pymysql.cursors.DictCursor
     )
